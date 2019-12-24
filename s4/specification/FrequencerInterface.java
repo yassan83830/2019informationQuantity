a912c3620d70485125eb13967c363953f4654764
Takuma Yasumori <foo@example.com>
2019-12-24 17:39:18 +0900

    10 Frequencerの問題を修正

--- s4/specification/FrequencerInterface.java
+++ s4/specification/FrequencerInterface.java
@@ -21,30 +21,28 @@ public interface FrequencerInterface {
     /**
      * Get the frequency count of TARGET.
      *
-     * It returns -1 when TARGET is not set or TARGET's length is zero.
-     * It returns 0 when SPACE is not set or SPACE's length is zero.
-     * Otherwise, returns the frequency of TAGET in SPACE
-     * which are most resently specified by setTaget and setSpace.
-     * If either target[] or space[] is modified after setTaget, or setSpace,
-     * the behavior is undefined
+     * It returns -1 when TARGET is not set or TARGET's length is zero. It returns 0
+     * when SPACE is not set or SPACE's length is zero. Otherwise, returns the
+     * frequency of TAGET in SPACE which are most resently specified by setTaget and
+     * setSpace. If either target[] or space[] is modified after setTaget, or
+     * setSpace, the behavior is undefined
      *
      * @return frequency count
      */
     int frequency();
 
     /**
-     * Get the frequency count of subBytes of TARGET in SPACE, which are 
-     * most resently specified by setTaget or set Space.
+     * Get the frequency count of subBytes of TARGET in SPACE, which are most
+     * resently specified by setTaget or set Space.
      *
-     * i.e. target[start], taget[start+1], ... , target[end-1].
-     * For the incorrect value of START or END, the behavior is undefined.
-     * If target[] or space[] is modified after setTaget, or setSpace,
-     * the behavior is undefined
+     * i.e. target[start], taget[start+1], ... , target[end-1]. For the incorrect
+     * value of START or END, the behavior is undefined. If target[] or space[] is
+     * modified after setTaget, or setSpace, the behavior is undefined
      *
      * @param start position (starting with 0) where the head of subByte exists.
-     * @param end postion (starting with 0) where next byte to the  tail of subByte exists.
+     * @param end   postion (starting with 0) where next byte to the tail of subByte
+     *              exists.
      * @return frequency count in subBytes.
      */
     int subByteFrequency(int start, int end);
 }
-
